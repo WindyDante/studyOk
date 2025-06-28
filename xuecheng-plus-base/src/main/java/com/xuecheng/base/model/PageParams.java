ca7f3b19de801c62207de2b666d7387a00ff5fd8
WindyDante <125888129+WindyDante@users.noreply.github.com>
2025-06-28 10:25:39 +0800

    feat: 设计接口及实体类，添加swagger

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
@@ -0,0 +1,27 @@
+package com.xuecheng.base.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class PageParams {
+
+    // 当前页码
+    @ApiModelProperty("当前页码")
+    private Long pageNo = 1L;
+
+    // 每页记录数默认值
+    @ApiModelProperty("每页记录数")
+    private Long pageSize = 10L;
+
+    public PageParams() {
+    }
+
+    public PageParams(Long pageNo, Long pageSize) {
+        this.pageNo = pageNo;
+        this.pageSize = pageSize;
+    }
+}
