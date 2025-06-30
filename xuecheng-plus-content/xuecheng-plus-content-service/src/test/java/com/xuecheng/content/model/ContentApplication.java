f77ff2e77e7ff94583121f9a4f942770c69190a3
WindyDante <125888129+WindyDante@users.noreply.github.com>
2025-06-30 11:29:01 +0800

    feat: 添加mapper相关代码，修复lombok不兼容的问题

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/model/ContentApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng.content.model;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.xuecheng.content.mapper") // 扫描Mapper接口所在的包
+public class ContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentApplication.class, args);
+    }
+}
