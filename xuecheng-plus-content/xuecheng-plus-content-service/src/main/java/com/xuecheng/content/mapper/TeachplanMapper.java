0723226a10d25330a88e6ea4d21d9290f297a6ce
WindyDante <125888129+WindyDante@users.noreply.github.com>
2025-07-12 21:36:20 +0800

    feat: 添加课程计划服务和控制器，支持课程计划树型结构查询

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
@@ -1,16 +1,20 @@
 package com.xuecheng.content.mapper;
 
-import com.xuecheng.content.model.po.Teachplan;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Repository;
+import com.xuecheng.content.model.dto.TeachplanDto;
+import com.xuecheng.content.model.po.Teachplan;
 
-/**
- * <p>
- * 课程计划 Mapper 接口
- * </p>
- *
- * @author eastwind
- */
+import java.util.List;
+
 public interface TeachplanMapper extends BaseMapper<Teachplan> {
+
+    /**
+     * @description 查询某课程的课程计划，组成树型结构 
+     * @param courseId
+     * @return com.xuecheng.content.model.dto.TeachplanDto
+     * @author Mr.M
+     * @date 2022/9/9 11:10
+    */
+    public List<TeachplanDto> selectTreeNodes(long courseId);
 
 }
